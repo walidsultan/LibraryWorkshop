051b73cab1b1db821c0ad3ba513fc6d847327e29
Walid Sultan <walidalysultan@gmail.com>
2015-07-02 12:13:21 -0500

    Added author class

---
+++ src/libraryWorkshop/models/Author.java
@@ -0,0 +1,20 @@
+package libraryWorkshop.models;
+
+public class Author extends Person {
+
+	public String credentials;
+	
+	
+	public String getCredentials() {
+		return credentials;
+	}
+
+
+	public void setCredentials(String credentials) {
+		this.credentials = credentials;
+	}
+
+
+	private static final long serialVersionUID = -3506645484707943700L;
+
+}
